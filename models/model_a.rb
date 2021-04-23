34f55a7d39e4fbcad9a5de9ddcf05f97fa853e19
Carina Sweet <carina.sweet06@gmail.com>
2021-04-23 12:26:56 -0600

    Add school model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  name = "Black Hills State",
+  city = "Spearfish",
+  enrollment = 3684,
+  percentage_female = 63.0,
+  tennis_program = false,
+  volleyball_program = true,
+  undergradute_programs = ["Human Services", "Education", "Biology"],
+  graduate_programs = ["Accounting", "Leadership", "Genomics"]
+}
