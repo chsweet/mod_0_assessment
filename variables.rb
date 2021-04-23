1c334ab15eae0498ba3874c32414d1d827211dd7
Carina Sweet <carina.sweet06@gmail.com>
2021-04-23 12:09:24 -0600

    Complete program specific challengee

---
+++ variables.rb
@@ -0,0 +1,14 @@
+famous_person = "Ryan Reynolds"
+
+is_active = true
+
+famous_person = "Ryan Gosling"
+
+num_3 = 3
+num_9 = 9
+sum = num_3 + num_9
+
+animals = ["deer", "elk", "moose"]
+animals.delete_at(0)
+
+p famous_person.length
