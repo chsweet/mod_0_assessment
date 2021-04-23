974728bea0a1146838779826add9d5b13e98a9db
Carina Sweet <carina.sweet06@gmail.com>
2021-04-23 12:35:44 -0600

    Add pet model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house_pet = {
+  pet_type = "Dog",
+  type_breed = "Dane Shepard",
+  age = 7,
+  number_of_naps = 5
+  is_cute = true,
+  trained = true,
+  nicknames = ["Koopaloop", "Kooper", "Kooperton the Third"],
+  favorite_toy = ["tennis ball", "squeaky duck", "stuffed elephant"]
+}
